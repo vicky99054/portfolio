affb683d1df3ef0c6f16c6f8a3aa0812453291cc
VICKY KUMAR <vickykumar.ara10@gmail.com>
2026-03-27 22:58:47 +0530

    Add typing effect for developer roles in script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed("#typing",{
+
+strings:[
+"Java Backend Developer",
+"Spring Boot Developer",
+"Microservices Enthusiast",
+"Kafka | Docker | REST APIs"
+],
+
+typeSpeed:60,
+backSpeed:40,
+loop:true
+
+});
